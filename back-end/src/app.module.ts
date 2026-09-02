3c8b78933bf5296f2c7741b667e80ca682cd3162
benoit-cht <benoit.challat@gmail.com>
2026-09-02 16:23:45 +0200

    feat(othello-controller): create a first controler for request Test

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { OthelloModule } from './othello/othello.module';
+
 @Module({
-  imports: [],
+  imports: [OthelloModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- back-end/src/othello/othello.controller.ts
+++ back-end/src/othello/othello.controller.ts
@@ -1,14 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post, Patch } from '@nestjs/common';
 import { OthelloService } from './othello.service';
+
+import type { MoveResult, Move, GameResult } from './interfaces/move-result.interface';
 
 @Controller('othello')
 export class OthelloController {
 
-  constructor(private readonly othelloService: OthelloService) {}
+    constructor(private readonly othelloService: OthelloService) {}
 
   // TODO: POST /othello/games (créer une partie)
   // TODO: GET /othello/games/:id (état d'une partie)
   // TODO: POST /othello/games/:id/moves (jouer un coup)
+    
+    @Get('create')
+    async creatGame()   {
+        
+        const userId = 'HostUser' ;
+        return( this.othelloService.createGame(userId) );
+    }
+    
+    @Get('join/:gameId')
+    async joinGame( @Param('gameId') gameId: string ) {
+        
+        const userId = 'VisitorUser'
+        return( this.othelloService.joinGame(gameId, userId) );
+        
+    }
+    
+    @Patch(':gameId/:userId/move')
+    async playMove(
+      @Param('gameId') gameId: string,
+      @Param('userId') userId: string,
+      @Body() move: Move,
+    ) {
+      return( this.othelloService.playMove(gameId, userId, move));
+    }
+    
+    @Get('state/:gameId')
+    async getState( @Param('gameId') gameId: string ){
+        
+        return( this.othelloService.getState(gameId) );
+    }
+    
 
 /*  @Get('ping')
   ping() {
