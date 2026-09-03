f72a5ecbc85c5c904f8cb18fdf608bb7c1c93475
maguerin <maguerin@student.42.fr>
2026-09-03 15:04:02 +0200

    infra(front-end): add basic configuration files

---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+	return (
+		<div className="bg-cyan-500 text-white p-4 italic">
+			Salut ca va ? Je suis le front-end de l'application Transcendence.
+		</div>
+	);
+};
+
+export default App
